63278c87c82d500f0a2af9af32b23498b981973f
Alex Stocking <alex.i.stocking@gmail.com>
2023-01-02 21:06:51 -0500

    Test attack and death

--- Scripts/EnemyBase.gd
+++ Scripts/EnemyBase.gd
@@ -1,17 +1,21 @@
 extends KinematicBody
 
+var maxHealth: int = 3
+var currentHealth = maxHealth
 
 var moveSpeed: float = 2.0
 var turnSpeed: float = 90.0
 
 var gravity: float = 4.0
 
+var justHit: bool = false
+
 enum states {IDLE, TURNING, MOVING, ATTACKING}
 var currentState
 
 var forward: Vector3 = Vector3(0, 0, 0)
 
-onready var animPlayer = get_node("AnimationPlayer")
+onready var animPlayer = get_node("Graphics/AnimationPlayer")
 onready var timer = get_node("Timer")
 
 
@@ -29,10 +33,29 @@ func _physics_process(delta):
 
 func changeState():
 	forward = transform.basis.z * moveSpeed
+	#iterate through the states until reaching the end, then loop back
 	if(currentState == states.MOVING):
 		currentState = states.IDLE
 	else:
 		currentState = currentState + 1
 
+func getHit():
+	if(justHit):
+		pass
+	currentHealth -= 1
+	timer.paused = true
+	animPlayer.play("hurt_test")
+	yield(animPlayer, "animation_finished")
+	checkDeath()
+	timer.paused = false
+	justHit = true
+
+func checkDeath():
+	if(currentHealth >= 0):
+		queue_free()
+
 func _on_Timer_timeout():
 	changeState()
+
+func _on_hurtBox_area_entered(area):
+	getHit()
